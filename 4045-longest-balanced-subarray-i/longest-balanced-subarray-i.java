2e407f5882af871666d5f0e6cabc63af67f1b912
Bitu Kumar <152399673+Bitu1269@users.noreply.github.com>
2026-02-11 13:13:08 +0530

    Time: 400 ms (16.55%) | Memory: 47.1 MB (45.77%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -1,26 +1,22 @@
 class Solution {
-    private static int[] seen = new int[100001];
-    private static int leet = 0;
     public int longestBalanced(int[] nums) {
-        leet++; 
-        int n = nums.length;
-        int res = 0;
+        int n= nums.length;
+        int max=0;
 
-        for (int i = 0; i < n && n - i > res; i++) {
-            int[] A = new int[2];
-            int marker = (leet << 16) | (i + 1);
-            for (int j = i; j < n; j++) {
-                int val = nums[j];
-                if (seen[val] != marker) {
-                    seen[val] = marker;
-                    A[val & 1]++;
+        for(int i=0; i<n; i++){
+            int even=0, odd=0; // for even odd count
+            Set<Integer> set = new HashSet<>(); // for Unique element store
+            for(int j=i; j<n; j++){
+                if(!set.contains(nums[j])){
+                    if(nums[j] % 2 ==0) even++;
+                    else odd++;
+                    set.add(nums[j]);
                 }
-
-                if (A[0] == A[1])
-                    res = Math.max(res, j - i + 1);
+                if(even == odd){
+                    max= Math.max(max, j-i+1);
+                }
             }
         }
-
-        return res;
+        return max;
     }
 }

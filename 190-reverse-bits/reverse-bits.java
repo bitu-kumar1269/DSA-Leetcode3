51dcf1b86e4e82667a79dc7aa2bea3c098e7210f
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-02-16 08:34:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (96.57%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,11 +1,9 @@
 class Solution {
     public int reverseBits(int n) {
-        if(n==0 ) return 0;
-
         int res =0;
         for(int i=1; i<=32; i++){
             res <<= 1; // left sift operater use for add at end remove one bit
-            res = (res |(n&1)); // n& 1 use for find last bit
+            res |= (n&1); // n& 1 use for find last bit
             n>>=1; // right shift operater use for add zeo at first remove one bit
         }
         return res; // it will store my revrese bit

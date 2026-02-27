00cc07e402fea8512db5d369d88471ab9e6056df
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-02-27 08:41:18 +0530

    Time: 5 ms (100.00%) | Memory: 48.1 MB (54.84%) - LeetSync

---
+++ 3983-minimum-operations-to-equalize-binary-string/minimum-operations-to-equalize-binary-string.java
@@ -0,0 +1,56 @@
+class Solution {
+
+    private int ceil(int x, int y) {
+        return (x + y - 1) / y;
+    }
+
+    public int minOperations(String S, int K) {
+
+        int N = S.length();
+        int Z = 0;
+
+        // Count zeros
+        for (char c : S.toCharArray()) {
+            if (c == '0') Z++;
+        }
+
+        // Special case
+        if (N == K) {
+            if (Z == 0) return 0;
+            if (Z == N) return 1;
+            return -1;
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        // Case 1: Z even
+        if (Z % 2 == 0) {
+
+            int M = Math.max(
+                    ceil(Z, K),
+                    ceil(Z, N - K)
+            );
+
+            // Make M even
+            if (M % 2 == 1) M++;
+
+            ans = Math.min(ans, M);
+        }
+
+        // Case 2: Parity match
+        if (Z % 2 == K % 2) {
+
+            int M = Math.max(
+                    ceil(Z, K),
+                    ceil(N - Z, N - K)
+            );
+
+            // Make M odd
+            if (M % 2 == 0) M++;
+
+            ans = Math.min(ans, M);
+        }
+
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
